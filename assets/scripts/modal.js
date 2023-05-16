6021ad76a721ea6481b8da601d3c8ae519965ae2
Ebbi Shaghouei <ebbi40@gmail.com>
2023-05-16 18:02:51 +0400

    menu fact sheet slide show

--- assets/scripts/modal.js
+++ assets/scripts/modal.js
@@ -1,20 +1,27 @@
-// Get the modal
-var modal = document.getElementById("modal");
 
-// Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("pure-kush");
-var modalImg = document.getElementById("img01");
-var captionText = document.getElementById("caption");
-img.onclick = function () {
-    modal.style.display = "block";
-    modalImg.src = this.src;
-    captionText.innerHTML = this.alt;
-}
+document.addEventListener('click', function (e) {
+    e = e || window.event;
+    var target = e.target;
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+    if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal') {
+        if (target.hasAttribute('data-target')) {
+            var m_ID = target.getAttribute('data-target');
+            document.getElementById(m_ID).classList.add('open');
+            e.preventDefault();
+        }
+    }
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function () {
-    modal.style.display = "none";
-}
+    // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
+    if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal')) {
+        var modal = document.querySelector('[class="modal open"]');
+        modal.classList.remove('open');
+        e.preventDefault();
+    }
+}, false);
+
+document.body.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape') {
+        console.log(e);
+        document.querySelector('.close').click();
+    }
+});
