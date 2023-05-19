97cbd310a67d67316ecd60f08531d560ff2791f5
Ebbi Shaghouei <ebbi40@gmail.com>
2023-05-19 21:22:10 +0400

    mobile about us page

--- assets/scripts/dark-light.js
+++ assets/scripts/dark-light.js
@@ -2,28 +2,42 @@
 const rootElement = document.documentElement;
 const darkModeElement = document.getElementById("dark-mode");
 const lightModeElement = document.getElementById("light-mode");
+const savedMode = localStorage.getItem("displayMode");
+
+darkModeElement.addEventListener("click", (event) => {
+    setDarkMode(event);
+});
+lightModeElement.addEventListener("click", (event) => {
+    setLightMode(event);
+});
+
+if (savedMode == "darkMode") {
+    /*
+        console.log("displayMode", "darkMode");
+    */
+    setDarkMode();
+};
 
 function setDarkMode(event) {
     /*
-        console.log("setTheme", event);
-        console.log("mode", "dark-mode");
+        console.log("setDarkMode", event);
     */
     rootElement.setAttribute("class", "dark");
     darkModeElement.setAttribute("class", "display-none");
     lightModeElement.setAttribute("class", "");
 
+    localStorage.setItem("displayMode", "darkMode");
+
 }
 
 function setLightMode(event) {
     /*
-        console.log("setTheme", event);
-        console.log("mode", "light-mode");
+        console.log("setLightMode", event);
     */
     rootElement.setAttribute("class", "light");
     lightModeElement.setAttribute("class", "display-none");
     darkModeElement.setAttribute("class", "");
 
-}
+    localStorage.setItem("displayMode", "lightMode");
 
-darkModeElement.onclick = setDarkMode;
-lightModeElement.onclick = setLightMode;
+}
